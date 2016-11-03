330dcf7529e3163643fd64785a41df3a3d7a7f5d
Nima Afazel <c147780@05505337DT.HOSTED.LAC.COM>
2016-11-03 14:46:21 -0700

    Add BudgetedP Controller and Views

---
+++ ItemCDBMigrations/Controllers/BudgetedPController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ItemCDBMigrations.Models;
+
+namespace ItemCDBMigrations.Controllers
+{
+    public class BudgetedPController : Controller
+    {
+        private ICContext db = new ICContext();
+
+        // GET: BudgetedP
+        public ActionResult Index()
+        {
+            var tblPOSITIONBUDGETEDs = db.tblPOSITIONBUDGETEDs.Include(t => t.tblBud).Include(t => t.tblBudItemNum).Include(t => t.tblDivision).Include(t => t.tblFilled).Include(t => t.tblFunction).Include(t => t.tblOrd).Include(t => t.tblOrgCode).Include(t => t.tblSection).Include(t => t.tblSubItem).Include(t => t.tblUnit);
+            return View(tblPOSITIONBUDGETEDs.ToList());
+        }
+
+        // GET: BudgetedP/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblPOSITIONBUDGETED tblPOSITIONBUDGETED = db.tblPOSITIONBUDGETEDs.Find(id);
+            if (tblPOSITIONBUDGETED == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblPOSITIONBUDGETED);
+        }
+
+        // GET: BudgetedP/Create
+        public ActionResult Create()
+        {
+            ViewBag.BudBud = new SelectList(db.tblBuds, "BudCode", "BudDesc");
+            ViewBag.BudItemNum = new SelectList(db.tblBudItemNums, "BudItemNum", "BudItemDesc");
+            ViewBag.BudDivCode = new SelectList(db.tblDivisions, "DivCode", "DivDesc");
+            ViewBag.BudFilled = new SelectList(db.tblFilleds, "FilledCode", "FilledDesc");
+            ViewBag.BudFunction = new SelectList(db.tblFunctions, "FuncCode", "FuncDesc");
+            ViewBag.BudOrd = new SelectList(db.tblOrds, "OrdCode", "OrdDesc");
+            ViewBag.BudOrgCode = new SelectList(db.tblOrgCodes, "BudOrgCode", "BudOrgCodeDesc");
+            ViewBag.BudSecCode = new SelectList(db.tblSections, "SecCode", "SecDesc");
+            ViewBag.BudSubItem = new SelectList(db.tblSubItems, "SubItemCode", "SubItemDesc");
+            ViewBag.BudUnitCode = new SelectList(db.tblUnits, "UnitCode", "UnitDesc");
+            return View();
+        }
+
+        // POST: BudgetedP/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BudPosNum,BudItemNum,BudSubItem,BudOrd,BudBud,BudFilled,BudFunction,BudOrgCode,BudDivCode,BudSecCode,BudUnitCode,Comments,SSMA_TimeStamp")] tblPOSITIONBUDGETED tblPOSITIONBUDGETED)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblPOSITIONBUDGETEDs.Add(tblPOSITIONBUDGETED);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BudBud = new SelectList(db.tblBuds, "BudCode", "BudDesc", tblPOSITIONBUDGETED.BudBud);
+            ViewBag.BudItemNum = new SelectList(db.tblBudItemNums, "BudItemNum", "BudItemDesc", tblPOSITIONBUDGETED.BudItemNum);
+            ViewBag.BudDivCode = new SelectList(db.tblDivisions, "DivCode", "DivDesc", tblPOSITIONBUDGETED.BudDivCode);
+            ViewBag.BudFilled = new SelectList(db.tblFilleds, "FilledCode", "FilledDesc", tblPOSITIONBUDGETED.BudFilled);
+            ViewBag.BudFunction = new SelectList(db.tblFunctions, "FuncCode", "FuncDesc", tblPOSITIONBUDGETED.BudFunction);
+            ViewBag.BudOrd = new SelectList(db.tblOrds, "OrdCode", "OrdDesc", tblPOSITIONBUDGETED.BudOrd);
+            ViewBag.BudOrgCode = new SelectList(db.tblOrgCodes, "BudOrgCode", "BudOrgCodeDesc", tblPOSITIONBUDGETED.BudOrgCode);
+            ViewBag.BudSecCode = new SelectList(db.tblSections, "SecCode", "SecDesc", tblPOSITIONBUDGETED.BudSecCode);
+            ViewBag.BudSubItem = new SelectList(db.tblSubItems, "SubItemCode", "SubItemDesc", tblPOSITIONBUDGETED.BudSubItem);
+            ViewBag.BudUnitCode = new SelectList(db.tblUnits, "UnitCode", "UnitDesc", tblPOSITIONBUDGETED.BudUnitCode);
+            return View(tblPOSITIONBUDGETED);
+        }
+
+        // GET: BudgetedP/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblPOSITIONBUDGETED tblPOSITIONBUDGETED = db.tblPOSITIONBUDGETEDs.Find(id);
+            if (tblPOSITIONBUDGETED == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BudBud = new SelectList(db.tblBuds, "BudCode", "BudDesc", tblPOSITIONBUDGETED.BudBud);
+            ViewBag.BudItemNum = new SelectList(db.tblBudItemNums, "BudItemNum", "BudItemDesc", tblPOSITIONBUDGETED.BudItemNum);
+            ViewBag.BudDivCode = new SelectList(db.tblDivisions, "DivCode", "DivDesc", tblPOSITIONBUDGETED.BudDivCode);
+            ViewBag.BudFilled = new SelectList(db.tblFilleds, "FilledCode", "FilledDesc", tblPOSITIONBUDGETED.BudFilled);
+            ViewBag.BudFunction = new SelectList(db.tblFunctions, "FuncCode", "FuncDesc", tblPOSITIONBUDGETED.BudFunction);
+            ViewBag.BudOrd = new SelectList(db.tblOrds, "OrdCode", "OrdDesc", tblPOSITIONBUDGETED.BudOrd);
+            ViewBag.BudOrgCode = new SelectList(db.tblOrgCodes, "BudOrgCode", "BudOrgCodeDesc", tblPOSITIONBUDGETED.BudOrgCode);
+            ViewBag.BudSecCode = new SelectList(db.tblSections, "SecCode", "SecDesc", tblPOSITIONBUDGETED.BudSecCode);
+            ViewBag.BudSubItem = new SelectList(db.tblSubItems, "SubItemCode", "SubItemDesc", tblPOSITIONBUDGETED.BudSubItem);
+            ViewBag.BudUnitCode = new SelectList(db.tblUnits, "UnitCode", "UnitDesc", tblPOSITIONBUDGETED.BudUnitCode);
+            return View(tblPOSITIONBUDGETED);
+        }
+
+        // POST: BudgetedP/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BudPosNum,BudItemNum,BudSubItem,BudOrd,BudBud,BudFilled,BudFunction,BudOrgCode,BudDivCode,BudSecCode,BudUnitCode,Comments,SSMA_TimeStamp")] tblPOSITIONBUDGETED tblPOSITIONBUDGETED)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblPOSITIONBUDGETED).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BudBud = new SelectList(db.tblBuds, "BudCode", "BudDesc", tblPOSITIONBUDGETED.BudBud);
+            ViewBag.BudItemNum = new SelectList(db.tblBudItemNums, "BudItemNum", "BudItemDesc", tblPOSITIONBUDGETED.BudItemNum);
+            ViewBag.BudDivCode = new SelectList(db.tblDivisions, "DivCode", "DivDesc", tblPOSITIONBUDGETED.BudDivCode);
+            ViewBag.BudFilled = new SelectList(db.tblFilleds, "FilledCode", "FilledDesc", tblPOSITIONBUDGETED.BudFilled);
+            ViewBag.BudFunction = new SelectList(db.tblFunctions, "FuncCode", "FuncDesc", tblPOSITIONBUDGETED.BudFunction);
+            ViewBag.BudOrd = new SelectList(db.tblOrds, "OrdCode", "OrdDesc", tblPOSITIONBUDGETED.BudOrd);
+            ViewBag.BudOrgCode = new SelectList(db.tblOrgCodes, "BudOrgCode", "BudOrgCodeDesc", tblPOSITIONBUDGETED.BudOrgCode);
+            ViewBag.BudSecCode = new SelectList(db.tblSections, "SecCode", "SecDesc", tblPOSITIONBUDGETED.BudSecCode);
+            ViewBag.BudSubItem = new SelectList(db.tblSubItems, "SubItemCode", "SubItemDesc", tblPOSITIONBUDGETED.BudSubItem);
+            ViewBag.BudUnitCode = new SelectList(db.tblUnits, "UnitCode", "UnitDesc", tblPOSITIONBUDGETED.BudUnitCode);
+            return View(tblPOSITIONBUDGETED);
+        }
+
+        // GET: BudgetedP/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblPOSITIONBUDGETED tblPOSITIONBUDGETED = db.tblPOSITIONBUDGETEDs.Find(id);
+            if (tblPOSITIONBUDGETED == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblPOSITIONBUDGETED);
+        }
+
+        // POST: BudgetedP/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblPOSITIONBUDGETED tblPOSITIONBUDGETED = db.tblPOSITIONBUDGETEDs.Find(id);
+            db.tblPOSITIONBUDGETEDs.Remove(tblPOSITIONBUDGETED);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
