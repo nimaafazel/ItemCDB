f5d7edb8b989aeeac2cf23eb199875b550e377ca
Nima Afazel <c147780@05505337DT.HOSTED.LAC.COM>
2016-11-07 08:07:24 -0800

    Controller and Views for ActualP

---
+++ ItemCDBMigrations/Controllers/ActualPController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ItemCDBMigrations.Models;
+
+namespace ItemCDBMigrations.Controllers
+{
+    public class ActualPController : Controller
+    {
+        private ICContext db = new ICContext();
+
+        // GET: ActualP
+        public ActionResult Index()
+        {
+            var tblPOSITIONACTUALs = db.tblPOSITIONACTUALs.Include(t => t.tblEMPLOYEELIST).Include(t => t.tblEmploymentType).Include(t => t.tblPayPeriod).Include(t => t.tblPOSITIONBUDGETED).Include(t => t.tblStep);
+            return View(tblPOSITIONACTUALs.ToList());
+        }
+
+        // GET: ActualP/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblPOSITIONACTUAL tblPOSITIONACTUAL = db.tblPOSITIONACTUALs.Find(id);
+            if (tblPOSITIONACTUAL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblPOSITIONACTUAL);
+        }
+
+        // GET: ActualP/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmplID = new SelectList(db.tblEMPLOYEELISTs, "EmplID", "LastName");
+            ViewBag.EmplType = new SelectList(db.tblEmploymentTypes, "EmplType", "EmplTypeDesc");
+            ViewBag.PayPeriod = new SelectList(db.tblPayPeriods, "PayTypeID", "PayTypeDesc");
+            ViewBag.ActPosNum = new SelectList(db.tblPOSITIONBUDGETEDs, "BudPosNum", "BudItemNum");
+            ViewBag.Step = new SelectList(db.tblSteps, "Step", "Step");
+            return View();
+        }
+
+        // POST: ActualP/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ActPosAutoID,ActPosNum,EmplID,EffectiveDate,DeptHireDate,ActEmplStatus,ActItemNum,ActSubItem,Step,PayRate,PayPeriod,EmplType,PreItemNum,PreSubItem,ReasonOfChange,Comments,SSMA_TimeStamp")] tblPOSITIONACTUAL tblPOSITIONACTUAL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblPOSITIONACTUALs.Add(tblPOSITIONACTUAL);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmplID = new SelectList(db.tblEMPLOYEELISTs, "EmplID", "LastName", tblPOSITIONACTUAL.EmplID);
+            ViewBag.EmplType = new SelectList(db.tblEmploymentTypes, "EmplType", "EmplTypeDesc", tblPOSITIONACTUAL.EmplType);
+            ViewBag.PayPeriod = new SelectList(db.tblPayPeriods, "PayTypeID", "PayTypeDesc", tblPOSITIONACTUAL.PayPeriod);
+            ViewBag.ActPosNum = new SelectList(db.tblPOSITIONBUDGETEDs, "BudPosNum", "BudItemNum", tblPOSITIONACTUAL.ActPosNum);
+            ViewBag.Step = new SelectList(db.tblSteps, "Step", "Step", tblPOSITIONACTUAL.Step);
+            return View(tblPOSITIONACTUAL);
+        }
+
+        // GET: ActualP/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblPOSITIONACTUAL tblPOSITIONACTUAL = db.tblPOSITIONACTUALs.Find(id);
+            if (tblPOSITIONACTUAL == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmplID = new SelectList(db.tblEMPLOYEELISTs, "EmplID", "LastName", tblPOSITIONACTUAL.EmplID);
+            ViewBag.EmplType = new SelectList(db.tblEmploymentTypes, "EmplType", "EmplTypeDesc", tblPOSITIONACTUAL.EmplType);
+            ViewBag.PayPeriod = new SelectList(db.tblPayPeriods, "PayTypeID", "PayTypeDesc", tblPOSITIONACTUAL.PayPeriod);
+            ViewBag.ActPosNum = new SelectList(db.tblPOSITIONBUDGETEDs, "BudPosNum", "BudItemNum", tblPOSITIONACTUAL.ActPosNum);
+            ViewBag.Step = new SelectList(db.tblSteps, "Step", "Step", tblPOSITIONACTUAL.Step);
+            return View(tblPOSITIONACTUAL);
+        }
+
+        // POST: ActualP/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ActPosAutoID,ActPosNum,EmplID,EffectiveDate,DeptHireDate,ActEmplStatus,ActItemNum,ActSubItem,Step,PayRate,PayPeriod,EmplType,PreItemNum,PreSubItem,ReasonOfChange,Comments,SSMA_TimeStamp")] tblPOSITIONACTUAL tblPOSITIONACTUAL)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblPOSITIONACTUAL).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmplID = new SelectList(db.tblEMPLOYEELISTs, "EmplID", "LastName", tblPOSITIONACTUAL.EmplID);
+            ViewBag.EmplType = new SelectList(db.tblEmploymentTypes, "EmplType", "EmplTypeDesc", tblPOSITIONACTUAL.EmplType);
+            ViewBag.PayPeriod = new SelectList(db.tblPayPeriods, "PayTypeID", "PayTypeDesc", tblPOSITIONACTUAL.PayPeriod);
+            ViewBag.ActPosNum = new SelectList(db.tblPOSITIONBUDGETEDs, "BudPosNum", "BudItemNum", tblPOSITIONACTUAL.ActPosNum);
+            ViewBag.Step = new SelectList(db.tblSteps, "Step", "Step", tblPOSITIONACTUAL.Step);
+            return View(tblPOSITIONACTUAL);
+        }
+
+        // GET: ActualP/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblPOSITIONACTUAL tblPOSITIONACTUAL = db.tblPOSITIONACTUALs.Find(id);
+            if (tblPOSITIONACTUAL == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblPOSITIONACTUAL);
+        }
+
+        // POST: ActualP/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblPOSITIONACTUAL tblPOSITIONACTUAL = db.tblPOSITIONACTUALs.Find(id);
+            db.tblPOSITIONACTUALs.Remove(tblPOSITIONACTUAL);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
