4712fd850f723fc51eafb5d615a178b1447249ec
Nima Afazel <c147780@05505337DT.HOSTED.LAC.COM>
2016-11-07 11:00:45 -0800

    Add Attributes and Annotations to ActualP

--- ItemCDBMigrations/Models/tblPOSITIONACTUAL.cs
+++ ItemCDBMigrations/Models/tblPOSITIONACTUAL.cs
@@ -12,46 +12,59 @@ namespace ItemCDBMigrations.Models
         [Key]
         public int ActPosAutoID { get; set; }
 
+        [Display(Name ="Position Number")]
         public int ActPosNum { get; set; }
 
+        [Display(Name ="Employee ID")]
         public int EmplID { get; set; }
 
+        [Display(Name ="Effective Date")]
         [Column(TypeName = "datetime2")]
         public DateTime EffectiveDate { get; set; }
 
+        [Display(Name ="Hire Date")]
         [Column(TypeName = "datetime2")]
         public DateTime? DeptHireDate { get; set; }
 
         [Required]
         [StringLength(1)]
+        [Display(Name ="Status")]
         public string ActEmplStatus { get; set; }
 
         [Required]
         [StringLength(10)]
+        [Display(Name ="Item Number")]
         public string ActItemNum { get; set; }
 
         [Required]
         [StringLength(1)]
+        [Display(Name ="SubItem Number")]
         public string ActSubItem { get; set; }
 
         [StringLength(50)]
         public string Step { get; set; }
 
         [Column(TypeName = "money")]
+        [Display(Name ="Pay Rate")]
         public decimal? PayRate { get; set; }
 
+        [Display(Name ="Pay Period")]
         public int? PayPeriod { get; set; }
 
         [Required]
         [StringLength(1)]
+        [Display(Name ="Employment Type")]
         public string EmplType { get; set; }
 
         [StringLength(10)]
+        [Display(Name ="PreItem Number")]
         public string PreItemNum { get; set; }
 
         [StringLength(1)]
+        [Display(Name ="PreSubItem Number")]
         public string PreSubItem { get; set; }
 
+        [Display(Name ="Reason of Change")]
         public string ReasonOfChange { get; set; }
 
         public string Comments { get; set; }
