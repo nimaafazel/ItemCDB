96d78ccb17222537840f08c49ce04e5ce4cc2449
Nima Afazel <c147780@05505337DT.HOSTED.LAC.COM>
2016-11-08 08:35:32 -0800

    Attributes on SubItem Model

--- ItemCDBMigrations/Models/tblSubItem.cs
+++ ItemCDBMigrations/Models/tblSubItem.cs
@@ -20,6 +20,7 @@ namespace ItemCDBMigrations.Models
         public string SubItemCode { get; set; }
 
         [StringLength(50)]
+        [Display(Name ="SubItem")]
         public string SubItemDesc { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
