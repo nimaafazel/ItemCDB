9f1ce0843b7de8a7c6802c33ab288022abc8e168
Nima Afazel <c147780@05505337DT.HOSTED.LAC.COM>
2016-11-08 08:34:16 -0800

    Attributes on OrgCode Model

--- ItemCDBMigrations/Models/tblOrgCode.cs
+++ ItemCDBMigrations/Models/tblOrgCode.cs
@@ -19,6 +19,7 @@ namespace ItemCDBMigrations.Models
         public string BudOrgCode { get; set; }
 
         [StringLength(30)]
+        [Display(Name ="Budgeted Org Code")]
         public string BudOrgCodeDesc { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
