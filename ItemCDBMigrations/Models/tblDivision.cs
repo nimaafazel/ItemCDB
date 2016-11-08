0e4ecfdf0918313228183692386e3d7224d3d626
Nima Afazel <c147780@05505337DT.HOSTED.LAC.COM>
2016-11-07 16:41:08 -0800

    Add Attributes to Division

--- ItemCDBMigrations/Models/tblDivision.cs
+++ ItemCDBMigrations/Models/tblDivision.cs
@@ -20,12 +20,15 @@ namespace ItemCDBMigrations.Models
         public int DivCode { get; set; }
 
         [StringLength(50)]
+        [Display(Name ="Division Code")]
         public string DivDesc { get; set; }
 
         [StringLength(50)]
+        [Display(Name ="Division Name")]
         public string DivDesc1 { get; set; }
 
         [StringLength(50)]
+        [Display(Name ="Division Chief")]
         public string DivChief { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
