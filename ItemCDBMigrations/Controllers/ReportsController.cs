b20964e054b4a81df3e8c821eb71d683eba30e37
Nima Afazel <c147780@05505337DT.HOSTED.LAC.COM>
2016-11-17 14:04:50 -0800

    Add Controller and view for reports

---
+++ ItemCDBMigrations/Controllers/ReportsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ItemCDBMigrations.Controllers
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Call to our aspx page to show the report.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LocalReport()
+        {
+            return Redirect("../ReprtVwr/ReprtVwr.aspx?repLoc=local");
+        }
+    }
+}
