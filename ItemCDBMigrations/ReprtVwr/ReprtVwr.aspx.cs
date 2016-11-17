85d1d0f5b7bba01fe95a7e327455c83a4d75b261
Nima Afazel <c147780@05505337DT.HOSTED.LAC.COM>
2016-11-17 13:57:01 -0800

    Add simple code to show report on asp page. Not showing anythting yet.

--- ItemCDBMigrations/ReprtVwr/ReprtVwr.aspx.cs
+++ ItemCDBMigrations/ReprtVwr/ReprtVwr.aspx.cs
@@ -11,7 +11,50 @@ namespace ItemCDBMigrations.ReprtVwr
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                // check the parameters to see which report are we going to show
+                string param = Request["repLoc"];
 
+                // load the report after the page loads
+                if (!string.IsNullOrEmpty(param))
+                {
+                    /*
+                    if (param == "local")
+                        showLocalReport();
+                    if (param == "remote")
+                        showRemoteReport();
+                        */
+                }
+            }
+        }
+
+        private void showLocalReport()
+        {
+            try
+            {
+                // report url         
+                string localReportPath = "LocalReports/VacancyByDiv.rdlc";
+
+                /*
+                // processing mode
+                rptViewer.ProcessingMode = ProcessingMode.Local;
+
+                // set the report path
+                rptViewer.LocalReport.ReportPath = localReportPath;
+
+                var dataSource = new VacancyDataSetTableAdapters.View_VacancyByDivisionOrderedTableAdapter().GetData();
+                rptViewer.LocalReport.DataSources.Add(new ReportDataSource("VacancyDataSet", (object)dataSource));
+
+                // refresh the report
+                rptViewer.LocalReport.Refresh();
+                */
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
         }
     }
 }
